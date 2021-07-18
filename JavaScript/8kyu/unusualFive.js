7c53402dec4439450b261293e70d84529a1c585e
da8865892q <da8865892q@gmail.com>
2021-07-18 19:56:09 +0800

    5 without numbers

---
+++ JavaScript/8kyu/unusualFive.js
@@ -0,0 +1,12 @@
+// 5 without numbers !!
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+// My code
+function unusualFive() {
+  return ['', '', '', '', ''].length 
+}
+
+// Best code
+// function unusualFive() {
+//   return 'five!'.length
+// }
