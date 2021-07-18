396788625d1b9ceaa93cdc53ff833bde25a3f651
da8865892q <da8865892q@gmail.com>
2021-07-19 01:42:05 +0800

    A Needle in the Haystack

---
+++ JavaScript/8kyu/findNeedle.js
@@ -0,0 +1,9 @@
+// A Needle in the Haystack
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+// My code
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`
+}
+
+// Best code
---
+++ Python/8kyu/findNeedle.py
@@ -0,0 +1,8 @@
+# A Needle in the Haystack
+# https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+# My code
+def find_needle(haystack):
+    return ('found the needle at position %s'%(haystack.index('needle')))
+
+# Best code
